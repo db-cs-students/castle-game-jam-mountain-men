791accf9afcdbbe5c3d869093a3fa32a352007f5
haydenrussell <69857685+haydenrussell@users.noreply.github.com>
2020-09-28 10:21:21 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
-Title:
-Creators:
-Description:
+Title:Mountian Master
+Creators: Hayden and Brady
+Description: A adventure to reach the castle and find the 
+treasure, make your way up a frosty Mountian that many have tried to take
+on.
 """
