90c87692360f2de1f53b5d0330beecc7b1819742
dbstudent13 <69854836+dbstudent13@users.noreply.github.com>
2020-10-01 09:55:54 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ scene.set_tile_map(img("""
     ............................................................................7...........................................................................................................................
     ....................................................................................ffffffffffffffffffff................................................................................................
     ..........................................................................7......ffffaaaaaaaaaaaaaaaaaaffff.............................................................................................
-    .....................fffff......................................................ffaaaaaaaaaaaaaa9aaaaaaaaafff.................................................................................ffff......
-    .............................................................................fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaff................................................................................faafffffff
-    ...........................................................................ffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafff.............fff..............ffff...........................................faaaaaaaaaa
-    ....................ffffff.....7......7..........................7......fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaffff.........ffaff...........fffaaaf........................................fffaaaaaaaaaa
+    .....................fffff......................................................ffaaaaaaaaaaaaaa9aaaaaaaaafff...........................................................................................
+    .............................................................................fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaff.................................................................................fffffffff
+    ...........................................................................ffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafff.............fff..............ffff............................................faaaaaaaaa
+    ....................ffffff.....7......7..........................7......fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaffff.........ffaff...........fffaaaf.........................................ffaaaaaaaaaa
     ...................faaaaaaff.................ffffff.....7..............ffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaff......fffaaaffff........faaaaaaf.......................................faaaaaaaaaaaa
-    .......7..........faaaaaaaaffff.............ffaaaaaff................fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafffffffaaaaaaaaaf........faaaaaaf......................................faaaaaaaaaaaaa
-    .............ffffffaaaaaaaaaaaaf...........ffaaaaaaaff......7.......ffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafffff....faaaaaaf.....................ffff............faaaaaaaaaaaaaa
-    fffffffffffffaaaaaaaaaaaaaaaaaafffffffffffffaaaaaa9aaaff.......fffffaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaff..faaaaaaaf.....................faaf..........ffaaaaaaaaaaaaaaa
+    .......7..........faaaaaaaaffff.............ffaaaaaff................fffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafffffffaaaaaaaaaf........faaaaaaf.......................................faaaaaaaaaaaa
+    .............ffffffaaaaaaaaaaaaf...........ffaaaaaaaff......7.......ffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafffff....faaaaaaf.....................ffff.............faaaaaaaaaaaaa
+    fffffffffffffaaaaaaaaaaaaaaaaaafffffffffffffaaaaaa9aaaff.......fffffaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaff..faaaaaaaf.....................faaf............ffaaaaaaaaaaaaa
     aaaaaaaaaaaaaaaaaaaaaaaaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaff.....ffaaaaaaaaaaaaaaaaaaaaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafffaaaaaaaafff...................faaff..........faaaaaaaaaaaaaaa
     aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9aaaaaaaaaaaaaaaaaafffffffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaf.......ff.........faaaaf..........faaaaaaaaaaaaaaa
     aaaaaaaaaaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaa9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaffffffffafffff....ffaaaaf.........ffaaaaaaaaaaaaaaa
@@ -121,21 +121,21 @@ scene.set_tile(10, img("""
 ## sprite control and creation/location
 mountianman1= sprites.create(img("""
     . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . f . . f . . . . . . . .
-    . . . . . f f . . . . . . . . .
-    . . . . . f f . . . . . . . . .
-    . . . . f . . f . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
+    . . . . . . f f f f f f . . . .
+    . . . . f f e e e e f 2 f . . .
+    . . . f f e e e e f 2 2 2 f . .
+    . . . f e e e f f e e e e f . .
+    . . . f f f f e e 2 2 2 2 e f .
+    . . . f e 2 2 2 f f f f e 2 f .
+    . . f f f f f f f e e e f f f .
+    . . f f e 4 4 e b f 4 4 e e f .
+    . . f e e 4 d 4 1 f d d e f . .
+    . . . f e e e 4 d d d d f . . .
+    . . . . 4 d d e 4 4 4 e f . . .
+    . . . . e d d e 2 2 2 2 f . . .
+    . . . . f e e f 4 4 5 5 f f . .
+    . . . . f f f f f f f f f f . .
+    . . . . . f f . . . f f f . . .
 """))
 controller.move_sprite(mountianman1)
 scene.camera_follow_sprite(mountianman1)
